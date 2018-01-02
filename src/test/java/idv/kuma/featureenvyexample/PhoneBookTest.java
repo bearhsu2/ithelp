28d509f7a3f7df440b388c6dc93b5f7df0dbf9c8
KUMA-PC\bearhsu2 <bearhsu2@gmail.com>
2018-01-03 01:01:23 +0800

    decouple the test!!!

--- src/test/java/idv/kuma/featureenvyexample/PhoneBookTest.java
+++ src/test/java/idv/kuma/featureenvyexample/PhoneBookTest.java
@@ -1,29 +1,36 @@
 package idv.kuma.featureenvyexample;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
-
 /**
  * Created by bearhsu2 on 1/2/2018.
  */
 public class PhoneBookTest {
+
+    private final int LINE_LENGTH = 10;
+
     @Test
     public void testGenerateFormattedPrint() throws Exception {
 
         // Prepare data
+        Contact mockedContact = Mockito.mock(Contact.class);
+        String fakeLine = RandomStringUtils.randomAlphanumeric(LINE_LENGTH);
+        Mockito.doReturn(fakeLine).when(mockedContact).generateFormattedPrint();
+
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addContact(new Contact("Kuma", "kuma@aaa.bbb", "123456789"));
-        phoneBook.addContact(new Contact("Maku", "maku@ccc.ddd", "987654321"));
+        phoneBook.addContact(mockedContact);
+        phoneBook.addContact(mockedContact);
 
         // Execute method
         String result = phoneBook.generateFormattedPrint();
 
         // Check result
-        String expected = "Kuma: kuma@aaa.bbb | 123456789. \nMaku: maku@ccc.ddd | 987654321. \n";
-
+        String expected = fakeLine + "\n" + fakeLine + "\n";
         assertEquals(expected, result);
-        System.out.println(result);
 
+        System.out.println(result);
     }
 }
