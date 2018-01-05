dfe4a4a8b747a7205d36ac4ae712866f26d0d020
KUMA-PC\bearhsu2 <bearhsu2@gmail.com>
2018-01-05 20:52:14 +0800

    singleton

---
+++ src/main/java/idv/kuma/singletonexample/ok/EagerInitialization.java
@@ -0,0 +1,17 @@
+package idv.kuma.singletonexample.ok;
+
+/**
+ * Created by bearhsu2 on 10/10/2016.
+ */
+
+// In eager initialization, the instance of Singleton Class is created at the time of class loading.
+public class EagerInitialization {
+
+    private EagerInitialization() {}
+
+    private static EagerInitialization eager = new EagerInitialization();
+
+    public static EagerInitialization getInstance(){
+        return eager;
+    }
+}
